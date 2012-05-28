5852977457da26581e1b889c98c20c879b9fa476
Dorrene Brown <dorreneb@gmail.com>
2012-05-28 18:58:38 -0400

    Added access to testdb for user.

--- setup/teardowndb.sql
+++ setup/teardowndb.sql
@@ -1,4 +1,5 @@
 DROP DATABASE IF EXISTS mentoringmatchdb;
+DROP DATABASE IF EXISTS test_mentoringmatchdb;
 
 /*Give silly privelages to mentoringmatch to make sure it exists 
 before deleting it* -- silly mysql with its lack of good user existance 
