78462083a11889b7fa50e77b25c1390d498c4ca5
Dorrene Brown <dorreneb@gmail.com>
2012-05-28 15:50:09 -0400

    Refactored dev-setup to be much more flexible with arguments. In addition, its now way easier to specify if you want to autoinitialize the mentoring superuser or not.

--- setup/dev-setup.sh
+++ setup/dev-setup.sh
@@ -1,12 +1,63 @@
 #!/bin/bash -e
+# Much thanks to http://rsalveti.wordpress.com/2007/04/03/bash-parsing-arguments-with-getopts/ for the getopts tutorial
 
-if [ $# -ne 2 ]
+usage()
+{
+cat << EOF
+usage: $0 options
+
+Given a MySQL root username and password, this script will set up and 
+initialize the mentoring match application.
+
+OPTIONS:
+   -h      Show this message
+   -u      MySQL root username
+   -p      MySQL root password
+   -m      Manually enter Django superuser credentials 
+   
+EOF
+}
+
+
+USERNAME=
+PASSWORD=
+SYNCDBOPTION="--noinput"
+
+# grab all options
+while getopts “mhu:p:” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         u)
+             USERNAME=$OPTARG
+             ;;
+         p)
+             PASSWORD=$OPTARG
+             ;;
+         m)
+             SYNCDBOPTION=""
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+#check to make sure all required data is entered
+if [[ -z $USERNAME ]] || [[ -z $PASSWORD ]]
 then
-  echo "Usage: `basename $0` msqlusername mysqlpassword"
-  exit -1
+     echo -e "\tError: Missing username/password.\n"
+     usage
+     exit 1
 fi
 
+#huzzah, everything is awesome. Run the script.
 echo "Creating project user and database..."
-mysql -u$1 -p$2 < setupdb.sql
+mysql -u$USERNAME -p$PASSWORD < setupdb.sql
 
-../mentoringmatch/manage.py syncdb --noinput
+echo "Syncing project to database..."
+../mentoringmatch/manage.py syncdb $SYNCDBOPTION
