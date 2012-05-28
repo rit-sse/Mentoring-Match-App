663c4bc83da3364558ae8e4e11ad1871c2fd02cd
Dorrene Brown <dorreneb@gmail.com>
2012-05-28 18:26:26 -0400

    Prettified the whitelist admin interface and added search functionality.

--- mentoringmatch/whitelist/admin.py
+++ mentoringmatch/whitelist/admin.py
@@ -1,4 +1,11 @@
 from whitelist.models import Mentor
 from django.contrib import admin
 
-admin.site.register(Mentor)
+class MentorAdmin(admin.ModelAdmin):
+  #Rearrange fields in the administrative panel
+  fields = ['first_name', 'last_name', 'dce']
+  #List fields in the admin listing nicely
+  list_display = ('dce', 'first_name', 'last_name')
+  search_fields = ['dce', 'first_name', 'last_name']
+
+admin.site.register(Mentor, MentorAdmin)
