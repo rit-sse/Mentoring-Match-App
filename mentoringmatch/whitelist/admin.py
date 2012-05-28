ad94692e4b2a5178fff97318ba6a05d6d1cb08b3
Dorrene Brown <dorreneb@gmail.com>
2012-05-28 17:58:16 -0400

    Updated gitignore to ignore temporary files (*~). In addition, a mentoring whitelist now exists that the superuser can add mentors to.

---
+++ mentoringmatch/whitelist/admin.py
@@ -0,0 +1,4 @@
+from whitelist.models import Mentor
+from django.contrib import admin
+
+admin.site.register(Mentor)
--- mentoringmatch/whitelist/models.py
+++ mentoringmatch/whitelist/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Mentor(models.Model):
+  dce = models.CharField(max_length=7)
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
