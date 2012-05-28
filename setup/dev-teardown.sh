5decace25e316ef969d4dffec069baffd988227e
Dorrene Brown <dorreneb@gmail.com>
2012-05-28 16:01:47 -0400

    Updated dev-teardown to be consistent with dev-setup.

--- setup/dev-teardown.sh
+++ setup/dev-teardown.sh
@@ -1,10 +1,56 @@
 #!/bin/bash -e
+# Much thanks to http://rsalveti.wordpress.com/2007/04/03/bash-parsing-arguments-with-getopts/ for the getopts tutorial
 
-if [ $# -ne 2 ]
+usage()
+{
+cat << EOF
+usage: $0 options
+
+Given a MySQL root username and password, this script will remove all users 
+and data associated with the mentoring match app program.
+
+OPTIONS:
+   -h      Show this message
+   -u      MySQL root username
+   -p      MySQL root password
+   
+EOF
+}
+
+
+USERNAME=
+PASSWORD=
+SYNCDBOPTION="--noinput"
+
+# grab all options
+while getopts “mhu:p:” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         u)
+             USERNAME=$OPTARG
+             ;;
+         p)
+             PASSWORD=$OPTARG
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+#check to make sure all required data is entered
+if [[ -z $USERNAME ]] || [[ -z $PASSWORD ]]
 then
-  echo "Usage: `basename $0` mysqlusername mysqlpassword"
-  exit -1
+     echo -e "\tError: Missing username/password.\n"
+     usage
+     exit 1
 fi
 
+#huzzah, everything is awesome. Run the script.
 echo "Destroying project user and database..."
-mysql -u$1 -p$2 < teardowndb.sql
+mysql -u$USERNAME -p$PASSWORD < teardowndb.sql
