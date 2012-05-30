fb0b01a0674b216fb52f5a37c6402c25f233f73a
Derek Erdmann <derek@derekerdmann.com>
2012-05-30 21:38:11 +0100

    Add validation tests for schedules.models

--- mentoringmatch/schedules/tests.py
+++ mentoringmatch/schedules/tests.py
@@ -1,48 +1,141 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from whitelist.models import Mentor
 from schedules.models import Template, Slot, Schedule, Assignment
 from datetime import datetime
 
-class SchedulesOutputTests( TestCase ):
+class TemplateTests( TestCase ):
+    """Tests schedules.models.Template"""
 
-    def test_overridden_template_unicode( self ):
-        """Ensures the template unicode contains useful info"""
-        template = Template( name="Some template" )
-        self.assertNotEqual( str( template ), 'Template object' )
+    def setUp( self ):
+        self.template = Template( name="Some template" )
+
+    def test_overridden_unicode( self ):
+        """Ensures the unicode method isn't the default"""
+        self.assertNotEqual( str( self.template ), 'Template object' )
+
+    def test_name_exists_valid( self ):
+        """Ensures that a template that contains only a name is valid.
+
+        No exception should be thrown.
+        """
+        self.template.full_clean()
+
+    def test_name_missing_invalid( self ):
+        """Ensures that a template that is missing a name should be invalid
+
+        No exception should be thrown.
+        """
+        self.template.name = None
+        with self.assertRaises( ValidationError ):
+            self.template.full_clean()
 
 
-    def test_overridden_slot_unicode( self ):
+class SlotTests( TestCase ):
+    """Tests schedules.models.Slot"""
+
+    def setUp( self ):
+        self.pattern = Template.objects.create( name="Some template" )
+        self.begin, self.finish = datetime.now(), datetime.now()
+        self.slot = Slot( template=self.pattern, 
+            start=self.begin, end=self.finish, day="MON" )
+
+    def test_overridden_unicode( self ):
         """Ensures the slot unicode is not default"""
-        pattern = Template.objects.create( name="Some template" )
-        begin, finish = datetime.now(), datetime.now()
+        self.assertNotEqual( str( self.slot ), 'Slot object' )
 
-        slot = Slot( template=pattern, start=begin, end=finish, day="MON" )
+    def test_allfields_valid( self ):
+        """Tests that a Slot with all fields is valid.
+        
+        No exception should be thrown"""
+        self.slot.full_clean()
 
-        self.assertNotEqual( str( slot ), 'Slot object' )
+    def test_template_missing_invalid( self ):
+        """Tests that a Slot with no template is invalid"""
+        with self.assertRaises( ValueError ):
+            self.slot.template = None
 
+    def test_start_missing_invalid( self ):
+        """Tests that a Slot with no start time is invalid"""
+        self.slot.start = None
+        with self.assertRaises( ValidationError ):
+            self.slot.full_clean()
+
+    def test_end_missing_invalid( self ):
+        """Tests that a Slot with no end time is invalid"""
+        self.slot.end = None
+        with self.assertRaises( ValidationError ):
+            self.slot.full_clean()
+
+    def test_end_missing_invalid( self ):
+        """Tests that a Slot with no day is invalid"""
+        self.slot.day = None
+        with self.assertRaises( ValidationError ):
+            self.slot.full_clean()
+
+
+class ScheduleTests( TestCase ):
+    """Tests schedules.models.Schedule"""
+
+    def setUp( self ):
+        self.pattern = Template.objects.create( name="Some template" )
+        self.begin, self.finish = datetime.now(), datetime.now()
+        self.sched = Schedule( template=self.pattern,
+            start=self.begin, end=self.finish )
     
-    def test_overridden_schedule_unicode( self ):
+    def test_overridden_unicode( self ):
         """Ensures the schedule unicode is not default"""
+        self.assertNotEqual( str( self.sched ), 'Schedule object' )
 
-        pattern = Template.objects.create( name="Some template" )
-        begin, finish = datetime.now(), datetime.now()
+    def test_template_missing_invalid( self ):
+        """Tests that a Schedule with no template is invalid"""
+        with self.assertRaises( ValueError ):
+            self.sched.template = None
 
-        sched = Schedule( template=pattern, start=begin, end=finish )
+    def test_start_missing_invalid( self ):
+        """Tests that a Schedule with no start time is invalid"""
+        self.sched.start = None
+        with self.assertRaises( ValidationError ):
+            self.sched.full_clean()
 
-        self.assertNotEqual( str( sched ), 'Schedule object' )
+    def test_end_missing_invalid( self ):
+        """Tests that a Schedule with no end time is invalid"""
+        self.sched.end = None
+        with self.assertRaises( ValidationError ):
+            self.sched.full_clean()
 
 
-    def test_overridden_assignment_unicode( self ):
-        """Ensures the assignment unicode is not default"""
+class AssignmentTests( TestCase ):
+    """Tests schedules.models.Assignment"""
 
-        person = Mentor.objects.create( dce="abc1234",
+    def setUp( self ):
+        self.person = Mentor.objects.create( dce="abc1234",
             first_name="Bob", last_name="Smith" )
-        pattern = Template.objects.create( name="Some template" )
-        begin, finish = datetime.now(), datetime.now()
-        place = Slot( template=pattern, start=begin, end=finish, day="MON" )
-        sched = Schedule( template=pattern, start=begin, end=finish )
+        self.pattern = Template.objects.create( name="Some template" )
+        self.begin, self.finish = datetime.now(), datetime.now()
+        self.sched = Schedule( template=self.pattern,
+            start=self.begin, end=self.finish )
+        self.place = Slot( template=self.pattern,
+            start=self.begin, end=self.finish, day="MON" )
 
-        assignment = Assignment( mentor=person, slot=place, schedule=sched )
+        self.assignment = Assignment( mentor=self.person,
+            slot=self.place, schedule=self.sched )
 
-        self.assertNotEqual( str( assignment ), 'Schedule object' )
+    def test_overridden_unicode( self ):
+        """Ensures the assignment unicode is not default"""
+        self.assertNotEqual( str( self.assignment ), 'Schedule object' )
 
+    def test_mentor_missing_invalid( self ):
+        """Tests that a Schedule with no template is invalid"""
+        with self.assertRaises( ValueError ):
+            self.assignment.mentor = None
+
+    def test_slot_missing_invalid( self ):
+        """Tests that a Schedule with no template is invalid"""
+        with self.assertRaises( ValueError ):
+            self.assignment.slot = None
+
+    def test_schedule_missing_invalid( self ):
+        """Tests that a Schedule with no template is invalid"""
+        with self.assertRaises( ValueError ):
+            self.assignment.schedule = None
