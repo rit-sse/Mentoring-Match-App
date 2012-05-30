90aee5edfe4b41d35ae46e6d2f04c9f124d69f97
Derek Erdmann <derek@derekerdmann.com>
2012-05-30 03:30:45 +0100

    Create initial session models and admin pages
    Models can be managed through the admin interface, although the order
    for creating objects isn't exactly obvious, and terminology isn't clear.
    The next step is to start building up to the admin UI in the sessions
    mockups.
    
    Also, Django admin pages are magic. Like Rails magic, except you can
    look at the source and understand it.

---
+++ mentoringmatch/mentoringmatch/sessions/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from mentoringmatch.sessions.models import *
+
+admin.site.register( Template )
+admin.site.register( Slot )
+admin.site.register( Schedule )
+admin.site.register( Assignment )
--- mentoringmatch/mentoringmatch/sessions/models.py
+++ mentoringmatch/mentoringmatch/sessions/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Template( models.Model ):
+    """Defines the layout of slots for any particular week.
+
+    For example, a "normal week" template may exist for most of the weeks in
+    the academic quarter, and a separate "finals week" template could account
+    for special scheduling during finals week.
+    """
+    
+    name = models.CharField( max_length=30 )
+
+    def __unicode__( self ):
+        return self.name
+
+
+class Slot( models.Model ):
+    """Represents a single time slot in the mentoring schedule. 
+
+    Example: One time slot may be on Tuesday, 10 AM to 12 PM
+    """
+
+    DAY_CHOICES = (
+        ('SUN', 'Sunday'),
+        ('MON', 'Monday'),
+        ('TUE', 'Tuesday'),
+        ('WED', 'Wednesday'),
+        ('THU', 'Thursday'),
+        ('FRI', 'Friday'),
+        ('SAT', 'Saturday'),
+    )
+
+    day = models.CharField( max_length=3, choices=DAY_CHOICES )
+    start = models.TimeField()
+    end = models.TimeField()
+    template = models.ForeignKey( Template )
+
+    def __unicode__( self ):
+        params = {
+            'day': self.get_day_display(),
+            'start': self.start,
+            'end': self.end
+        }
+        return "%(day)s - %(start)s to %(end)s" % params
+
+
+class Schedule( models.Model ):
+    """Applies a template to a range of weeks.
+    
+    Each week contained in the date range will have the slots defined in the
+    associated template.
+    """
+
+    start = models.DateField()
+    end = models.DateField()
+    template = models.ForeignKey( Template )
+
+    def __unicode__( self ):
+        params = {
+            'template': self.template,
+            'start': self.start,
+            'end': self.end
+        }
+        return "%(template)s - %(start)s to %(end)s" % params
+
+            
+class Assignment( models.Model ):
+    """Associates a mentor with a specific slot within a session"""
+
+    mentor = models.ForeignKey( User )
+    slot = models.ForeignKey( Slot )
+    template = models.ForeignKey( Template )
+
+    def __unicode__( self ):
+        params = {
+            'mentor': self.mentor.username,
+            'slot': self.slot,
+        }
+        return "%(mentor)s - %(slot)s" % params
