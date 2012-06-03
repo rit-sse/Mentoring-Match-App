3a82cd6ef311099bc12a33732c64f7ba9ad9c9ca
Derek Erdmann <derek@derekerdmann.com>
2012-06-03 18:34:10 +0100

    Create static calendar for schedule templates
    The template add/edit form will include a calendar display that enables
    easy modification of slots.  I created a static calendar template to
    build the initial styles and scripts for layout out items, which
    currently breaks the template 'add' view.
    
    Calendar styles also use Compass, a tool that compliles SCSS files into
    full CSS automatically during development, and also includes an
    incredibly useful library of mixins for CSS3 and other common tasks.
    It does require Ruby to compile any modified SCSS, but CSS files also
    get checked into the repository for simplicity during deployment.

--- mentoringmatch/schedules/admin.py
+++ mentoringmatch/schedules/admin.py
@@ -1,7 +1,52 @@
 from django.contrib import admin
-from schedules.models import *
+from django import forms
+from django.shortcuts import render
+from schedules.models import Template, Slot, Schedule, Assignment
+import logging
 
-admin.site.register( Template )
+
+class TemplateAdmin( admin.ModelAdmin ):
+
+    def add_view( self, request ):
+        
+        if not self.has_change_permission(request):
+            raise PermissionDenied
+
+        # Display from 8 AM to 8 PM (no label on end time)
+        time_range = list( range( 8, 20 ) )
+
+        def convert_time( time ):
+            """Generates a label for a 24-hour time"""
+            hour = time
+            if hour % 12 == 0:
+                hour = 12
+            else:
+                hour = hour % 12
+
+
+            return str( hour ) +" "+ ( "AM" if time // 12 == 0 else "PM" )
+
+        time_labels = map( convert_time, time_range )
+
+        return render( request, 'admin/schedules/template/edit_form.html', { 
+            'app_label': self.model._meta.app_label,
+            'opts': self.model._meta,
+            'change': False,
+            'is_popup': '_popup' in request.REQUEST,
+            'save_as': False,
+            'has_add_permission': True,
+            'has_delete_permission': False,
+            'has_change_permission': True,
+            'has_file_field': False,
+            'has_absolute_url': False,
+            'auto_populated_fields': (),
+            'add': True,
+            'time_range': time_range,
+            'time_labels': time_labels,
+        })
+
+
+admin.site.register( Template, TemplateAdmin )
 admin.site.register( Slot )
 admin.site.register( Schedule )
 admin.site.register( Assignment )
