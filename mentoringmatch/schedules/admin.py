75b49bf8f7d8770a3ad82d9b05513c358de5b8eb
Derek Erdmann <derek@derekerdmann.com>
2012-06-17 21:40:23 +0100

    Added inline assignment editing to schedule admin

--- mentoringmatch/schedules/admin.py
+++ mentoringmatch/schedules/admin.py
@@ -4,13 +4,27 @@ from django.shortcuts import render
 from schedules.models import Template, Slot, Schedule, Assignment
 import logging
 
+
 class SlotInline( admin.TabularInline ):
+    """Enables inline editing of template slots"""
     model = Slot
 
+
 class TemplateAdmin( admin.ModelAdmin ):
+    """Configures SlotInline for the Template admin page"""
     inlines = [ SlotInline ]
+
+
+class AssignmentInline( admin.TabularInline ):
+    """Enables inline editing of assignments in a schedule"""
+    model = Assignment
+
+
+class ScheduleAdmin( admin.ModelAdmin ):
+    """Configures the AssignmentInline on the Schedule admin page"""
+    inlines = [ AssignmentInline ]
 
 admin.site.register( Template, TemplateAdmin )
 admin.site.register( Slot )
-admin.site.register( Schedule )
+admin.site.register( Schedule, ScheduleAdmin )
 admin.site.register( Assignment )
