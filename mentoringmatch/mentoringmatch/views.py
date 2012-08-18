3f74e155dd38805ac7cb576e54afce4fc0737b15
Derek Erdmann <derek@derekerdmann.com>
2012-08-18 13:41:16 -0700

    Rename site.html to base.html

--- mentoringmatch/mentoringmatch/views.py
+++ mentoringmatch/mentoringmatch/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render_to_response
 import datetime
 
 def index(request):
-    return render_to_response('site.html', {
+    return render_to_response('base.html', {
         'title': 'SSE Mentoring Match',
     })
